7c310d5da82086ff17aaa530d3dcf4bc56037b47
robotto <mark@moore.dk>
2023-01-13 13:51:52 +0100

    Tydeligere fejlkode. ;)

--- viTesterInput.py
+++ viTesterInput.py
@@ -5,11 +5,11 @@ try:
     etTal = int(input('Giv mig et tal: '))
 except:
     print("det virkede ikke")
-    exit('ID10T')
+    exit('Fejlkode: ID-10-T')
 
 etAndetTal = int(input('Giv mig et andet tal: '))
 
 summen = etTal+etAndetTal
 print('Summen af de to tal er: ', summen, ',', navnet, '.. Vidste du ikke det?')
 
-print()11
+print()
