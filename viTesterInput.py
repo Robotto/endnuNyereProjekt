627421eca56eaa33add3a9df790ff073739a1063
robotto <mark@moore.dk>
2023-02-22 11:39:27 +0100

    Jeg har lavet floatfunktionen lidt mere universel, så man kan kalde den med en string som den skal spørge brugeren om (som parameter)

--- viTesterInput.py
+++ viTesterInput.py
@@ -1,16 +1,20 @@
-def giverAltidEnFloat():
-    tastet = input("giv mig et tal")
+#Tager imod et spørgsmål som parameter, som er dét den spørger brugeren om.
+#Kalder sig selv rekursivt indtil brugeren har indtastet en gyldig værdi som kan parses til en float.
+def giverAltidEnFloat(askThisQuestion):
+    tastet = input(askThisQuestion)
     try:
         return float(tastet)
     except ValueError:
-        print("det virkede ikke.. prøv igen...")
-        return giverAltidEnFloat()
+        print(f"Du tastede {tastet}... Det lykkedes ikke at tolke som et tal. Prøv igen...")
+        return giverAltidEnFloat() #https://media.tenor.com/C_typNW6OgEAAAAC/recursion-winnie.gif
 
+alder = giverAltidEnFloat("Hvad er din alder?")
 
 tallene = []
 antalTal = 5
+print(f"Jeg skal bruge {antalTal} tal.")
 for i in range( antalTal ):
-    tallene.append( giverAltidEnFloat() )
+    tallene.append( giverAltidEnFloat("Stik mig et tal, bro! ") )
 
 #de skal lægges sammen og printes
-print(f"fedt. Tak for fem tal.. Summen er: {sum(tallene)}")
+print(f"fedt. Tak for fem tal, bro.. Summen er: {sum(tallene)}")
